6d70f51d3779894019f78e4901133912a7e4ac60
Robert Pepple <bob.pepple@comcast.net>
2017-04-06 14:26:30 -0700

    Initial commit

---
+++ assets/app/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { LogoutComponent } from './logout.component';
+import { SigninComponent } from './signin.component';
+import { SignupComponent } from './signup.component';
+import { authRouting } from "./auth.routing";
+
+@NgModule({
+  declarations: [
+    LogoutComponent,
+    SignupComponent,
+    SigninComponent
+  ],
+  imports: [
+      CommonModule,
+      ReactiveFormsModule,
+      authRouting
+  ]
+})
+export class AuthModule {
+
+}
---
+++ assets/app/messages/message.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { MessagesComponent } from './messages.component';
+import { MessageInputComponent } from './message-input.component';
+import { MessageListComponent } from './message-list.component';
+import { MessageComponent } from './message.component';
+import { MessageService } from './message.service';
+
+@NgModule({
+  declarations: [
+    MessageComponent,
+    MessageListComponent,
+    MessageInputComponent,
+    MessagesComponent
+  ],
+  imports: [
+      CommonModule,
+      FormsModule
+  ],
+  providers: [MessageService]
+})
+export class MessageModule {
+
+}
